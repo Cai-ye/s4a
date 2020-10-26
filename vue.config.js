5781a038ed88263aaae4d5503df391adbeccb1a3
Cai-yejiao <51696628+Cai-ye@users.noreply.github.com>
2020-10-26 14:59:27 +0800

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -4,7 +4,7 @@ function resolve(dir) {
   return path.join(__dirname, dir)
 }
 module.exports = {
- 
+  publicPath:'/s4a/',
   configureWebpack: {
     // provide the app's title in webpack's name field, so that
     // it can be accessed in index.html to inject the correct title.
